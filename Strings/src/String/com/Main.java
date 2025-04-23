2b8e5261bc3ba9d6e9806944c58dd746fe8f88e4
pratiksha pandey <139357332+Pratikshapandey1609@users.noreply.github.com>
2025-04-23 22:48:37 +0530

    Add files via upload
    commit changes

---
+++ Strings/src/String/com/Main.java
@@ -0,0 +1,15 @@
+package String.com;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int [] arr = {2 , 3 , 4, 5,6,7,19};
+        int a = 10;
+
+        String name = "pratiksha";
+        System.out.println(name);
+
+    }
+}
---
+++ Strings/src/String/com/ValisAnagram.java
@@ -0,0 +1,10 @@
+package String.com;
+
+public class ValisAnagram {
+    public static void main(String[] args) {
+
+    }
+    public boolean isAnagram(String s, String t) {
+
+    }
+}
